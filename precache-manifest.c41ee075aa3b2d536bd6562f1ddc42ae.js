321df38b786f846f8581de535c9f6b600395f08e
weijun <weijunske@gmail.com>
2019-12-31 04:52:05 -0800

    Updates

---
+++ precache-manifest.c41ee075aa3b2d536bd6562f1ddc42ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78724f0d67dec6ec3eca3b11d030f97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "463dee3dc5fbb47336c1",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8456329e290296725f31",
+    "url": "/static/js/2.a9a89675.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a9a89675.chunk.js.LICENSE"
+  },
+  {
+    "revision": "463dee3dc5fbb47336c1",
+    "url": "/static/js/main.b2b2dea4.chunk.js"
+  },
+  {
+    "revision": "83a9cadc7ed32ef57e34",
+    "url": "/static/js/runtime-main.2997187c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
